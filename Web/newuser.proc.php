b19548f419ef34746fae36b232d9d972e536f6c9
Musta <8664.joan23@fje.edu>
2017-01-30 16:20:14 +0100

    newuser.proc v2

--- Web/newuser.proc.php
+++ Web/newuser.proc.php
@@ -2,13 +2,13 @@
 include("conexion.php");
 $nombre= $_POST['nombre'];
 $apellidos= $_POST['apellidos'];
-$email1= $_POST['email1'];
-$email2= $_POST['email2'];
+$email1= $_POST['correo_personal'];
+$email2= $_POST['correo_profesional'];
 $telf_personal= $_POST['telf_personal'];
 $telf_profesional= $_POST['telf_profesional'];
 $tipo_contacto= $_POST['tipo_contacto'];
 
-$query= "INSERT INTO tbl_contacts(recurso_nombre,recurso_tipo, recurso_disponible, recurso_foto) VALUES('$nombre','$apellidos','$disponible','$imagen') ";
+$query= "INSERT INTO tbl_contacts(nombre,apellidos, correo_personal, correo_profesional,  telf_personal, telf_profesional, tipo_contacto) VALUES('$nombre','$apellidos','$email1','$email2','$telf_personal','$telf_profesional','$tipo_contacto') ";
 $resultado= $conexion->query($query);
 
 if($resultado){
